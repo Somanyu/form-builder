78dce64ae4883f3ad5efc68c85424e57d94fa9a0
Somanyu Samal <somanyu.03samal@gmail.com>
2023-12-09 20:27:03 +0530

    Retrieve stats for forms using Prisma client

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,116 @@
-import Image from 'next/image'
+import { GetFormStats } from '@/actions/form';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Separator } from '@/components/ui/separator';
+import { Skeleton } from '@/components/ui/skeleton';
+import { ClockIcon, CursorArrowIcon, EyeOpenIcon, FileTextIcon } from '@radix-ui/react-icons';
+import { ReactNode, Suspense } from 'react';
 
 export default function Home() {
   return (
-    <div>Hello</div>
+    <div className="container pt-4">
+      <Suspense fallback={<StatsCards loading={true} />}>
+        <CardStatsWrapper />
+      </Suspense>
+      <Separator className="my-6" />
+      <h2 className="text-4xl font-bold col-span-2">Your forms</h2>
+      <Separator className="my-6" />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <FormCardSkeleton/>
+        <FormCardSkeleton/>
+        <FormCardSkeleton/>
+      </div>
+    </div>
+  )
+}
+
+async function CardStatsWrapper() {
+  const stats = await GetFormStats()
+  return <StatsCards loading={false} data={stats} />;
+}
+
+interface StatsCardProps {
+  data?: Awaited<ReturnType<typeof GetFormStats>>;
+  loading: boolean;
+}
+
+function StatsCards(props: StatsCardProps) {
+  const { data, loading } = props;
+
+  return (
+    <div className="w-full pt-8 gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+      <StatsCard
+        title='Total visits'
+        icon={<EyeOpenIcon className="text-blue-600" />}
+        helperText='All time form visits'
+        value={data?.visits.toLocaleString() || ""}
+        loading={loading}
+        className=''
+      />
+
+      <StatsCard
+        title="Total submissions"
+        icon={<FileTextIcon className="text-yellow-600" />}
+        helperText="All time form submissions"
+        value={data?.submissions.toLocaleString() || ""}
+        loading={loading}
+        className=""
+      />
+
+      <StatsCard
+        title="Submission rate"
+        icon={<CursorArrowIcon className="text-green-600" />}
+        helperText="Visits that result in form submission"
+        value={data?.submissionRate.toLocaleString() + "%" || ""}
+        loading={loading}
+        className=""
+      />
+
+      <StatsCard
+        title="Bounce rate"
+        icon={<ClockIcon className="text-red-600" />}
+        helperText="Visits that leaves without interacting"
+        value={data?.bounceRate.toLocaleString() + "%" || ""}
+        loading={loading}
+        className=""
+      />
+    </div>
+  )
+}
+
+
+export function StatsCard({
+  title, value, icon, helperText, loading, className
+}: {
+  title: string;
+  value: string;
+  icon: ReactNode;
+  helperText: string;
+  loading: boolean;
+  className: string;
+}) {
+  return (
+    <Card className={className}>
+      <CardHeader className="flex flex-row items-center justify-between pb-2">
+        <CardTitle className="text-sm font-medium text-muted-foreground">{title}</CardTitle>
+        {icon}
+      </CardHeader>
+      <CardContent>
+        <div className="text-2xl font-bold">
+          {loading && (
+            <Skeleton>
+              <span className="opacity-0">0</span>
+            </Skeleton>
+          )}
+          {!loading && value}
+        </div>
+        <p className="text-xs text-muted-foreground pt-1">{helperText}</p>
+      </CardContent>
+    </Card>
+  )
+}
+
+function FormCardSkeleton() {
+  return (
+    <Skeleton className='border-2 border-primary-/20 h-[190px] w-full'/>
   )
 }
